962c0b90d83e3b842aea07552512c3741031b17b
djumanov <djumanovdev@gmail.com>
2025-09-19 17:03:21 +0500

    crud

--- src/main.py
+++ src/main.py
@@ -5,24 +5,80 @@ from .models import User
 
 
 def add_user():
+
+    email = "samisamiyev@gmail.com"
     password = "1234"
+    first_name = "sami"
+    last_name = "samiyev"
+
+    db = LocalSession()
+    
+    user = db.query(User).filter(User.email == email).first()
+    if user:
+        print("user mavjud")
+        return
+   
     hashed_password = sha256(password.encode()).hexdigest()
     new_user = User(
-        first_name="ali",
-        last_name="valiyev",
-        email="alivaliyev@gmail.com",
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
         hashed_password=hashed_password
     )
 
-    db = LocalSession()
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
+def get_users():
+    db = LocalSession()
+
+    users = db.query(User).all()
+    print(users)
+
+def update_user():
+    db = LocalSession()
+
+    user = db.query(User).filter_by(email = "alivaliyev@gmail.com").first()
+    user.first_name = "Updated Ali"
+
+    db.commit()
+
+def delete_user():
+    db = LocalSession()
+
+    user = db.query(User).filter_by(email = "alivaliyev@gmail.com").first()
+    
+    db.delete(user)
+
+    db.commit()
+
+def login():
+    email = "valialiyev@gmail.com"
+    password = "1234"
+
+    hashed_password = sha256(password.encode()).hexdigest()
+
+    db = LocalSession()
+
+    user = db.query(User).filter(
+        User.email == email, 
+        User.hashed_password == hashed_password).first()
+    if user:
+        print("siz login qilindingiz")
+        return
+    
+    print("user topilmadi")
+
 
 def main():
 
-    add_user()
+    # add_user()
+    # update_user()
+    # delete_user()
+    # get_users()
+    login()
+
     
     Base.metadata.create_all(engine)
 
